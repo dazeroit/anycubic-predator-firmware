70854eba452ed7d963358392b3dbe7105d2d8f9a
ivan.maruca <ivan.maruca@gmail.com>
2020-12-22 19:31:10 +0100

    minor fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "PREDATOR"
+#define CUSTOM_MACHINE_NAME "Predator"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
